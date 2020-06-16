50171b270c90049007c6e6aa9e15c3a3e65cf0d2
Michaell-DANTE <66742681+Michaell-DANTE@users.noreply.github.com>
2020-06-16 11:33:49 -0500

    version 9
    correccion de proceso

--- controlador/entrada.php
+++ controlador/entrada.php
@@ -2,25 +2,7 @@
 
     $contenido = ("../vista/plantilla.phtml"); 
 
-    if( isset($_REQUEST['enviar']) )
-    {
 
-        require("../controlador/ingreso.php");
-
-        if( $nombre == "" || $pass == "" )
-        {
-
-    
-
-        }else{
-
-            header('Location: ../vista/principal.phtml');
-
-        }
-
-        
-
-    }
 
     require_once("../vista/plantilla.phtml");
 
